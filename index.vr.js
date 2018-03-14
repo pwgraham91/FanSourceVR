9c095b3b5e053daca665e5a3145ce02c8b37f9df
Peter Graham <pwgraham91@gmail.com>
2018-03-13 19:35:27 -0500

    omg its working

--- index.vr.js
+++ index.vr.js
@@ -22,27 +22,20 @@ export default class FanSourceVR extends React.Component {
       <View>
         <Pano source={asset('stadium_pano.jpg')}/>
 
-        {/*<Text*/}
-          {/*style={{*/}
-            {/*backgroundColor: '#777879',*/}
-            {/*fontSize: 0.8,*/}
-            {/*fontWeight: '400',*/}
-            {/*layoutOrigin: [0.5, 0.5],*/}
-            {/*paddingLeft: 0.2,*/}
-            {/*paddingRight: 0.2,*/}
-            {/*textAlign: 'center',*/}
-            {/*textAlignVertical: 'center',*/}
-            {/*transform: [{translate: [0, 0, -3]}],*/}
-          {/*}}>*/}
-          {/*hello*/}
-        {/*</Text>*/}
-        <Video
-          style={{height: 2.25, width: 4}}
-          source={asset('elephants-dream.webm')}
-          playerState={this.state.playerState} />
-        <VideoControl
-          style={{height: 0.2, width: 4}}
-          playerState={this.state.playerState} />
+        <View
+          style={{
+            alignItems: 'center',
+            layoutOrigin: [0.5, 0.5, 0],
+            transform: [{translate: [0, 0, -4]}],
+          }}>
+          <Video
+            style={{height: 2.25, width: 4}}
+            source={asset('elephants-dream.webm')}
+            playerState={this.state.playerState} />
+          <VideoControl
+            style={{height: 0.2, width: 4}}
+            playerState={this.state.playerState} />
+        </View>
       </View>
     );
   }
