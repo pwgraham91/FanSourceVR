0ce2fe01cc23679ed3832d67ae5acde92ddfe9b7
Peter Graham <pwgraham91@gmail.com>
2018-03-14 12:28:46 -0500

    show all videos

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -4,22 +4,40 @@ import './App.css';
 import FacebookVideo from './components/FacebookVideo'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {videos: []}
+  }
+
+  componentDidMount() {
+    fetch("https://api.crowdtangle.com/posts/?listIds=554797&types=live_video,live_video_complete,live_video_scheduled&sortBy=date&token=RAgQBEFe1ZILaKJKHvOwY78ingbYpPvhU7aeHhWu", {
+      headers: {
+        'content-type': 'application/json',
+      },
+      mode: 'cors',
+    })
+    .then(response => response.json())
+    .then(data => {
+      this.setState({
+        videos: data.result.posts
+      })
+    })
+    .catch(data => {
+      console.log('error', data)
+    });
+  }
+
   render() {
+    let videos = [];
+    for (let i=0; i < this.state.videos.length; i++) {
+      videos.push(<FacebookVideo key={this.state.videos[i].link} videoURL={this.state.videos[i].link} />)
+    }
     return (
       <div className="App">
         <h1>Fan Source VR</h1>
-
-
         <div className="fb-live">
-          <FacebookVideo videoID={1762190837170778} />
-          <FacebookVideo videoID={1762190837170778} />
-          <FacebookVideo videoID={1762190837170778} />
-          <FacebookVideo videoID={1762190837170778} />
-          <FacebookVideo videoID={1762190837170778} />
-          <FacebookVideo videoID={1762190837170778} />
-
+          {videos}
         </div>
-
       </div>
     );
   }
--- webapp/src/components/FacebookVideo.js
+++ webapp/src/components/FacebookVideo.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
 
 class FacebookVideo extends Component {
-  constructor(props) {
-    super(props);
-    this.videoURL = `https://www.facebook.com/facebook/videos/${this.props.videoID}/`
-  }
-
   render() {
     return (
-      <div className="fb-video" data-href={this.videoURL} data-width="500" data-show-text="false">
+      <div className="fb-video" data-href={this.props.videoURL} data-width="500" data-show-text="false">
         <div className="fb-xfbml-parse-ignore">
-          <blockquote cite={this.videoURL}>
-            <a href={this.videoURL}>How to Share With Just Friends</a>
+          <blockquote cite={this.props.videoURL}>
+            <a href={this.props.videoURL}>How to Share With Just Friends</a>
             <p>How to share with just friends.</p>
             Posted by <a href="https://www.facebook.com/facebook/">Facebook</a> on Friday, December 5, 2014
           </blockquote>
