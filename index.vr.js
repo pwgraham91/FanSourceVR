e4818462afaa43a0b447029a487a212d2d76d0dd
Peter Graham <pwgraham91@gmail.com>
2018-03-13 21:25:05 -0500

    better styles

--- index.vr.js
+++ index.vr.js
@@ -24,29 +24,29 @@ export default class FanSourceVR extends React.Component {
         <View
           style={{
             alignItems: 'center',
-            layoutOrigin: [1, 1, 0],
-            transform: [{translate: [0, 0, -4]}],
+            layoutOrigin: [2, 1, 0],
+            transform: [{translate: [0, 0, -8]}],
           }}>
           <Video
-            style={{height: 4.5, width: 8}}
+            style={{height: 2.25, width: 4}}
             source={asset('soccer/8th.mp4')}
             playerState={this.state.playerState} />
           <VideoControl
-            style={{height: 0.2, width: 8}}
+            style={{height: 0.2, width: 4}}
             playerState={this.state.playerState} />
         </View>
         <View
           style={{
             alignItems: 'center',
-            layoutOrigin: [0.5, 0.5, 0],
+            layoutOrigin: [0.5, 1.5, 0],
             transform: [{translate: [0, 0, -4]}],
           }}>
           <Video
-            style={{height: 4.5, width: 8}}
+            style={{height: 2.25, width: 4}}
             source={asset('soccer/1st.mp4')}
             playerState={this.state.playerState} />
           <VideoControl
-            style={{height: 0.2, width: 8}}
+            style={{height: 0.2, width: 4}}
             playerState={this.state.playerState} />
         </View>
         <View
@@ -56,11 +56,11 @@ export default class FanSourceVR extends React.Component {
             transform: [{translate: [0, 0, -4]}],
           }}>
           <Video
-            style={{height: 4.5, width: 8}}
+            style={{height: 2.25, width: 4}}
             source={asset('soccer/10th.mp4')}
             playerState={this.state.playerState} />
           <VideoControl
-            style={{height: 0.2, width: 8}}
+            style={{height: 0.2, width: 4}}
             playerState={this.state.playerState} />
         </View>
       </View>
