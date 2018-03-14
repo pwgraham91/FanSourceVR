3f98854b62008eaeda60c05d6a3d2c0c18dbaf32
Peter Graham <pwgraham91@gmail.com>
2018-03-13 19:30:21 -0500

    cars

--- index.vr.js
+++ index.vr.js
@@ -22,27 +22,29 @@ export default class FanSourceVR extends React.Component {
       <View>
         <Pano source={asset('stadium_pano.jpg')}/>
 
-        {/*<Text*/}
-          {/*style={{*/}
-            {/*backgroundColor: '#777879',*/}
-            {/*fontSize: 0.8,*/}
-            {/*fontWeight: '400',*/}
-            {/*layoutOrigin: [0.5, 0.5],*/}
-            {/*paddingLeft: 0.2,*/}
-            {/*paddingRight: 0.2,*/}
-            {/*textAlign: 'center',*/}
-            {/*textAlignVertical: 'center',*/}
-            {/*transform: [{translate: [0, 0, -3]}],*/}
-          {/*}}>*/}
-          {/*hello*/}
-        {/*</Text>*/}
-        <Video
-          style={{height: 2.25, width: 4}}
-          source={asset('elephants-dream.webm')}
-          playerState={this.state.playerState} />
-        <VideoControl
-          style={{height: 0.2, width: 4}}
-          playerState={this.state.playerState} />
+        <View
+          style={{
+            alignItems: 'center',
+            layoutOrigin: [0.5, 0.5, 0],
+            transform: [{translate: [0, 0, -4]}],
+          }}>
+          <Video
+            style={{height: 2.25, width: 4}}
+            source={[
+              asset('car/car-20120827-85.mp4', {format: 'mp4'}),
+              asset('car/car-20120827-86.mp4', {format: 'mp4'}),
+              asset('car/car-20120827-87.mp4', {format: 'mp4'}),
+              asset('car/car-20120827-88.mp4', {format: 'mp4'}),
+              asset('car/car-20120827-89.mp4', {format: 'mp4'}),
+              asset('car/car-20120827-8b.mp4', {format: 'mp4'}),
+              asset('car/car-20120827-8c.mp4', {format: 'mp4'}),
+              asset('car/car-20120827-8d.mp4', {format: 'mp4'}),
+              asset('car/car-20120827-manifest.mpd', {format: 'mpd'}),
+            ]}
+            playerState={this.state.playerState}
+          />
+          <VideoControl style={{height: 0.2, width: 4}} playerState={this.state.playerState} />
+        </View>
       </View>
     );
   }
--- vr/client.js
+++ vr/client.js
@@ -4,8 +4,11 @@
 
 // Auto-generated content.
 import {VRInstance} from 'react-vr-web';
+import {addCustomizedVideoPlayer} from 'react-vr-web';
+import DashVideoPlayer from "./DashVideoPlayer";
 
 function init(bundle, parent, options) {
+  addCustomizedVideoPlayer(DashVideoPlayer)
   const vr = new VRInstance(bundle, 'FanSourceVR', parent, {
     // Add custom options here
     ...options,
